1542a4cc6ad72d53ecebf3bd602897b324da6918
Hainf <973218319@qq.com>
2020-11-23 22:34:12 +0800

    add

--- flamingoserver/fileserversrc/FileServer.h
+++ flamingoserver/fileserversrc/FileServer.h
@@ -1,5 +1,5 @@
 /** 
- *  鏂囦欢鏈嶅姟鍣ㄤ富鏈嶅姟绫伙紝FileServer.h
+ *  文件服务器主服务类，FileServer.h
  *  zhangyl 2017.03.17
  **/
 #pragma once
@@ -21,6 +21,7 @@ struct StoredUserInfo
     std::string nickname;
 };
 
+// nocopy 
 class FileServer final
 {
 public:
@@ -30,20 +31,22 @@ public:
     FileServer(const FileServer &rhs) = delete;
     FileServer &operator=(const FileServer &rhs) = delete;
 
+    // 
     bool init(const char *ip, short port, EventLoop *loop, const char *fileBaseDir = "filecache/");
     void uninit();
 
 private:
-    //新连接到来调用或连接断开，所以需要通过conn->connected()来判断，一般只在主loop里面调用
+    // 新连接到来调用或连接断开，所以需要通过conn->connected()来判断，一般只在主loop里面调用
+    // 创建一个新会话， 设置可读事件回调函数
     void onConnected(std::shared_ptr<TcpConnection> conn);  
     
-    //连接断开
+    //连接断开, 这里只是移除了会话
     void onDisconnected(const std::shared_ptr<TcpConnection>& conn);
    
 
 private:
     std::unique_ptr<TcpServer>            m_server;     
     std::list<std::shared_ptr<FileSession>>      m_sessions;
-    std::mutex                    m_sessionMutex;    //澶氱嚎绋嬩箣闂翠繚鎶?m_sessions
-    std::string                    m_strFileBaseDir;   //鏂囦欢鐩?褰?
+    std::mutex                    m_sessionMutex;    //多线程之间保护m_sessions
+    std::string                    m_strFileBaseDir;   //文件目录
 };
